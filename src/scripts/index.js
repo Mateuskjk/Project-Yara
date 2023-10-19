18f5d07f8f047636056a9436e43d2e30e756d768
Mateuskjk <mcmateus1342@gamil.com>
2023-10-18 20:36:18 -0400

    Create login screen and logic of them

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -125,36 +125,14 @@ btn.addEventListener("click", function(e){
 	const classeName = inputClasse.value;
 	const passName = inputPass.value;
 
-	console.log(fromName)
-	console.log(toName)
-	console.log(dateIdaName)
-	console.log(dateVoltaName)
-	console.log(idaEVoltaName)
-	console.log(classeName)
-	console.log(passName)
+	//CRIANDO UM OBJETO COM AS INFORMAÇÕES CAPTURADAS NA SEARCH INPUT======================================================
+	const pesquisaInfo = {
+		fromName,
+		toName,
+		dateIdaName,
+		dateVoltaName,
+		idaEVoltaName,
+		classeName,
+		passName,
+	};
 })
-// ==============================================================
-
-
-
-
-// const sections4 = document.querySelectorAll('nav[id]')
-    
-// const scrollActive4 = () =>{
-//   	const scrollDown = window.scrollY
-
-// 	sections1.forEach(current =>{
-// 		const sectionHeight = current.offsetHeight,
-// 			  sectionTop = current.offsetTop - 58,
-// 			  sectionId = current.getAttribute('id'),
-// 			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-
-// 		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-// 			sectionsClass.classList.add('active-link')
-// 		}else{
-// 			sectionsClass.classList.remove('active-link')
-// 		}                                                    
-// 	})
-// }
-// window.addEventListener('scroll', scrollActive4)
-//=================================================================================================
---
+++ src/scripts/login.js
@@ -0,0 +1,23 @@
+// Captura valores dos inputs
+const btnLogin = document.querySelector("#enviar");
+
+btnLogin.addEventListener("click", function(e){
+  e.preventDefault();
+
+  const inputUserName = document.querySelector("#tagName");
+  const inputPassUser = document.querySelector("#pass");
+
+  const userName = inputUserName.value;
+  const passUser = inputPassUser.value;
+
+  // console.log(userName)
+  // console.log(passUser)
+
+})
+
+
+
+//=================================================================================================
+
+// ==============================================================
+//=================================================================================================
