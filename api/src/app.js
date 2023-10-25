70ac04271283bb02a576053d3857d48fbe4563e9
Mateus Silva e Silva <mateus.silva@vantiva.com>
2023-10-25 11:36:56 -0400

    Register page finished POST

--- api/src/app.js
+++ api/src/app.js
@@ -25,27 +25,7 @@ https.createServer({
 
 
 
-// function limitarBusca () {
-//   const express = require('express');
-//   const sqlite3 = require('sqlite3');
-//   const app = express();
-  
-//   const db = new sqlite3.Database('database.db'); // Substitua pelo nome do seu banco de dados
-  
-//   // Rota para obter a lista de destinos disponíveis
-//   app.get('/destinos', (req, res) => {
-//     db.all('SELECT * FROM destinos', (err, rows) => {
-//       if (err) {
-//         return res.status(500).json({ error: err.message });
-//       }
-//       res.json({ destinos: rows });
-//     });
-//   });
-  
-//   app.listen(3000, () => {
-//     console.log('API está executando na porta 3000');
-//   });
-// }
+
 
 
 
--- src/scripts/register.js
+++ src/scripts/register.js
@@ -1,45 +1,55 @@
-
 const btnAccount = document.querySelector('#send');
 
-btnAccount.addEventListener('click', function(e){
-    e.preventDefault();
+btnAccount.addEventListener('click', function (e) {
+  e.preventDefault();
 
-    const inputFirstName = document.querySelector('#nome');
-    const inputLastName = document.querySelector('#s-nome');
-    const inputEmail = document.querySelector('#email');
-    const inputConfEmail = document.querySelector('#c-email');
-    const inputSenha = document.querySelector('#passWrd');
-    const inputConfSenha =document.querySelector('#c-passWrd');
+  const inputFirstName = document.querySelector('#nome');
+  const inputLastName = document.querySelector('#s-nome');
+  const inputEmail = document.querySelector('#email');
+  const inputConfEmail = document.querySelector('#c-email');
+  const inputSenha = document.querySelector('#passWrd');
+  const inputConfSenha = document.querySelector('#c-passWrd');
 
+  const nome = inputFirstName.value;
+  const sobrenome = inputLastName.value;
+  const email = inputEmail.value;
+  const confEmail = inputConfEmail.value;
+  const senha = inputSenha.value;
+  const confSenha = inputConfSenha.value;
 
-    const fisrtName = inputFirstName.value;
-    const lastName = inputLastName.value;
-    const email = inputEmail.value;
-    const confEmail = inputConfEmail.value;
-    const senha = inputSenha.value;
-    const confSenha = inputConfSenha.value;
+  const registerUser = {
+    nome,
+    sobrenome,
+    email,
+    confEmail,
+    senha,
+    confSenha
+  };
 
-    const registerUser = {
-        fisrtName,
-        lastName,
-        email,
-        confEmail,
-        senha,
-        confSenha
-    };
+  const arqRegister = JSON.stringify(registerUser);
+  localStorage.setItem('registerUser', arqRegister);
 
-    const arqRegister = JSON.stringify(registerUser)
-	console.log(arqRegister)
+  window.location.href = "login.html"
 
-    // console.log(fisrtName)
-    // console.log(lastName)
-    // console.log(email)
-    // console.log(confEmail)
-    // console.log(senha)
-    // console.log(confSenha)
-})
-
-
-
-
-/*email, c-email, passWrd, c-passWrd*/
+  // Realize a solicitação POST para o servidor Node.js
+  fetch('http://localhost:3000/usuario', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(registerUser)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erro na solicitação. Status: ' + response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Erro:', error);
+    });
+  
+});
